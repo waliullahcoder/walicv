bdcdd4666aed2e7432efacb3356d413d6e303b10
waliullahcoder <waliullahbiplob786@gmail.com>
2025-12-28 07:32:01 +0600

    updated ref

--- cv.php
+++ cv.php
@@ -106,9 +106,33 @@ $details = $data2['details'] ?? [];
         </div>
       </div>
 
-      <div class="left-footer">
-        <?= $data['footer'] ?>
+       <div class="divider"></div>
+      <div class="section">
+  <div class="section-title">Personal Information</div>
+  <div class="divider"></div>
+
+  <?php foreach ($data['personal_info'] as $key => $value): ?>
+    <?php if ($key !== 'apps'): ?>
+      <div><strong><?= ucwords(str_replace('_', ' ', $key)) ?>:</strong> <?= $value ?></div>
+    <?php endif; ?>
+  <?php endforeach; ?>
+
+</div>
+
+
+      <div class="divider"></div>
+
+      <div class="section">
+        <div class="section-title">REFERENCES</div>
+        <div class="divider"></div>
+        <div class="contact">
+         <?php foreach ($data['references'] as $i => $ref): ?>
+            <div><strong><?= ($i+1) ?>. <?= $ref['name'] ?><br></strong> Designation: <?= $ref['designation'] ?><br> Internal Phone: <?= $ref['internal_phone'] ?><br> IP Landline: <?= $ref['ip_landline'] ?><br> IP Direct Dial: <?= $ref['ip_direct_dial'] ?><br> Email: <?= $ref['email'] ?></div>
+          <?php endforeach; ?>
+        </div>
       </div>
+
+      
     </aside>
 
     <section class="right">
@@ -152,19 +176,7 @@ $details = $data2['details'] ?? [];
         <?php endforeach; ?>
       </div>
 
-      <div class="divider"></div>
-      <div class="section">
-  <div class="section-title">Personal Information</div>
-  <div class="divider"></div>
-
-  <?php foreach ($data['personal_info'] as $key => $value): ?>
-    <?php if ($key !== 'apps'): ?>
-      <div><strong><?= ucwords(str_replace('_', ' ', $key)) ?>:</strong> <?= $value ?></div>
-    <?php endif; ?>
-  <?php endforeach; ?>
-
-</div>
-
+     
 
       <div class="divider"></div>
 
@@ -181,7 +193,9 @@ $details = $data2['details'] ?? [];
       <div class="divider"></div>
 
       <footer>
-        <small>References available on request · Last updated: <?= $data['last_updated'] ?></small>
+        <small><div class="left-footer">
+        <?= $data['footer'] ?>
+      </div></small>
       </footer>
     </section>
   </main>
